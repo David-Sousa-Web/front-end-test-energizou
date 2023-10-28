305f439b7af553b7c8e88979f2d091e4469fb610
David Sousa <david01sousa@hotmail.com>
2023-10-28 15:35:12 -0300

    feat: add header

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+import './header.css'
+
+export default function Header() {
+  return (
+    <header className="header-container">
+      <div>
+        <img src="https://d9hhrg4mnvzow.cloudfront.net/docs.energizou.com.br/energizou-app/8284e97f-logo-plataforma_1057018000000000000028.png" alt="" className="header-logo"/>
+      </div>
+      
+      
+      <h1 className='header-title'>Teste FullStack</h1>
+    </header>
+  )
+}
